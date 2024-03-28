9d336f200e57e3b21cbf30a14839ef76893eeba0
sathagesey33 <abdukdirmuhanad@gmail.com>
2024-03-28 22:30:43 +0300

    updated log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,46 @@
-
-    #!/usr/bin/python3
-"""
-Log parsing
-"""
+#!/usr/bin/env python3
 
 import sys
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+def print_stats(total_size, status_codes):
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes):
+        print(f"{code}: {status_codes[code]}")
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+
+def main():
+    total_size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403:
+                    0, 404: 0, 405: 0, 500: 0}
+    line_count = 0
 
     try:
         for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
+            parts = line.strip().split()
+            if len(parts) != 10:
+                continue
+
+            status_code = int(parts[8])
+            file_size = int(parts[9])
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+            total_size += file_size
+            line_count += 1
+
+            if line_count == 10:
+                print_stats(total_size, status_codes)
+                total_size = 0
+                status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403:
+                                0, 404: 0, 405: 0, 500: 0}
+                line_count = 0
+
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise                
+        print_stats(total_size, status_codes)
+        sys.exit(0)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
